c870a2f413b2f0b745a4b72e966570cdb37d979f
Raphael olimpio <76962348+raphaelolimpio@users.noreply.github.com>
2026-05-14 15:33:03 -0300

    Refactor next.config.ts for cleaner configuration
    Refactor next.config.ts to simplify image handling and retain basePath and assetPrefix settings.

--- next.config.ts
+++ next.config.ts
@@ -1,41 +1,17 @@
-import type {NextConfig} from 'next';
-
-const repoName = 'pc2';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-  trailingSlash: true,
-  /* config options here */
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/pc2',
+  assetPrefix: '/pc2/',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
